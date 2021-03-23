947254202ca89bcd671f1ef6e5b3c3af13181b70
ppate343 <71097041+ppate343@users.noreply.github.com>
2021-03-23 16:39:40 -0400

    enemy spider movement
    spider follows enemy

--- Assets/Scenes/Game Assets/__Scripts/Spider.cs
+++ Assets/Scenes/Game Assets/__Scripts/Spider.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Spider : Enemy
 {
     // Start is called before the first frame update
+
+    public float speed; 
+    private Transform target; 
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("PC").GetComponent<Transform>(); 
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position=Vector2.MoveTowards(transform.position, target.position, speed*Time.deltaTime);
     }
 }
