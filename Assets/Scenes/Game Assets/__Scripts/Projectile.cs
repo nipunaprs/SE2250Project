b98b04977bad76948dd0a0f261341876acb197fd
ppate343 <71097041+ppate343@users.noreply.github.com>
2021-03-23 23:42:18 -0400

    comments
    added comments to spider and projectile

--- Assets/Scenes/Game Assets/__Scripts/Projectile.cs
+++ Assets/Scenes/Game Assets/__Scripts/Projectile.cs
@@ -4,32 +4,37 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //variables 
     public float speed; 
     private Transform player; 
     private Vector2 target; 
     void Start()
     {
+        //setting player object to player 
         player = GameObject.FindGameObjectWithTag("PC").transform; 
+        //setting target movement to player position 
         target = new Vector2(player.position.x, player.position.y); 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //moves projectiles towards player position 
         transform.position = Vector2.MoveTowards(transform.position, target, speed*Time.deltaTime);
 
+        //if projectile reaches players position, projectile is destroyed 
         if(transform.position.x == target.x && transform.position.y == target.y) {
             DestroyProjectile(); 
 
         } 
     }
+    // projectile is destroyed on collision with player
     void OnTriggerEnter2D (Collider2D other) {
         if(other.CompareTag("PC")) {
             DestroyProjectile(); 
         }
     }
 
+    // destroying projectile 
     void DestroyProjectile() {
         Destroy(gameObject); 
     }
