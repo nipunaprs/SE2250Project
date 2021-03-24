f9149a24e8456df80eada6bcfa00218f55b8e75a
ppate343 <71097041+ppate343@users.noreply.github.com>
2021-03-23 21:10:52 -0400

    Update Spider.cs
    spider movement with player bounds

--- Assets/Scenes/Game Assets/__Scripts/Spider.cs
+++ Assets/Scenes/Game Assets/__Scripts/Spider.cs
@@ -17,6 +17,7 @@ public class Spider : Enemy
     // Update is called once per frame
     void Update()
     {
+       if(Vector2.Distance(transform.position, target.position)> 3)
         transform.position=Vector2.MoveTowards(transform.position, target.position, speed*Time.deltaTime);
     }
 }
