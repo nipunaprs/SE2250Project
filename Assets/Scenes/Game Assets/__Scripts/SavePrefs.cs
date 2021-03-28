b0e8cd0a37b754708299c1c92113aa9623391725
Nick Pysklywec <npysklyw@uwo.ca>
2021-03-28 13:26:30 -0400

    Comments

--- Assets/Scenes/Game Assets/__Scripts/SavePrefs.cs
+++ Assets/Scenes/Game Assets/__Scripts/SavePrefs.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class SavePrefs : MonoBehaviour
 {
+
+    //Tracks player choice in intro scene
     int powerChoice;
 
- 	
-
+//This is used on the button game object as a function to call when each button is clicked
 public void SetPowerChoice(int choice) {
     powerChoice = choice;
 }
  	
+//We can save the player choice at the end of the customization
 public void SaveGame()
 {
+    //Using playerprefs to save data and load it in the other scene
 	PlayerPrefs.SetInt("SavedInteger", powerChoice);
 	PlayerPrefs.Save();
+
+    //We proceed to load the first scene
     SceneManager.LoadScene (sceneName:"Level1");
 	
 }
