08a4b6cb746a03eb09fe5e797c14e17b53348e81
Nick Pysklywec <npysklyw@uwo.ca>
2021-04-10 17:31:11 -0400

    Healthbar Comments

--- Assets/Scenes/Game Assets/__Scripts/HealthBar.cs
+++ Assets/Scenes/Game Assets/__Scripts/HealthBar.cs
@@ -8,11 +8,14 @@ public class HealthBar : MonoBehaviour
 
     public Slider slider;
 
+    //Sets the max slider value to a value
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
     }
+    
+    //Sets the current slider value to a specific value
     public void SetHealth(int health)
     {
         slider.value = health;
