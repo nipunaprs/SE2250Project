bcdac8dc0ae0ffb1b085ccb0bd9944c5d59b6643
Nick Pysklywec <npysklyw@uwo.ca>
2021-04-10 17:29:24 -0400

    Commenting in XPBar

--- Assets/Scenes/Game Assets/__Scripts/XPBar.cs
+++ Assets/Scenes/Game Assets/__Scripts/XPBar.cs
@@ -9,26 +9,29 @@ public class XPBar : MonoBehaviour
     
     public Slider slider;
 
+    //Max XP set function
     public void SetMaxXP(int xp)
     {
         slider.maxValue = xp;
-        //slider.value = xp;
     }
 
+    //This will set the slider xp to a certain value
     public void SetXP(float xp)
     {
         slider.value = xp;
     }
 
+    //This will add a specific amount of xp to the current valur
     public void IncrementXP(float xp) {
         SetXP(slider.value + xp);
     }
 
+    //This will reset the xp to 0
     public void ResetXP() {
-        slider.value = 0;
-        
+        slider.value = 0;      
     }
 
+    //Returns if the slider is currently full
     public bool IsMax() {
         if (slider.maxValue == slider.value) {
             return true;
